aea15b121c24be512ba1587b0878cb5ebe448309
Devil200207 <74138871+Devil200207@users.noreply.github.com>
2024-03-26 19:09:36 +0530

    solution of week 3 + jwt updated

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,20 +1,21 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
+const z = require("zod");
 
+const emailShema = z.string().email();
+const PasswodSchema = z.string().min(6);
 
-/**
- * Generates a JWT for a given username and password.
- *
- * @param {string} username - The username to be included in the JWT payload.
- *                            Must be a valid email address.
- * @param {string} password - The password to be included in the JWT payload.
- *                            Should meet the defined length requirement (e.g., 6 characters).
- * @returns {string|null} A JWT string if the username and password are valid.
- *                        Returns null if the username is not a valid email or
- *                        the password does not meet the length requirement.
- */
 function signJwt(username, password) {
     // Your code here
+
+    const email = emailShema.safeParse(username);
+    const pass = PasswodSchema.safeParse(password);
+
+    if(!email.success || !pass.success) {
+        return null;
+    }
+    const signature = jwt.sign({username},jwtPassword);
+    return signature;
 }
 
 /**
@@ -27,6 +28,14 @@ function signJwt(username, password) {
  */
 function verifyJwt(token) {
     // Your code here
+
+    try{
+        const verify = jwt.verify(token, jwtPassword);
+        return true;
+    }
+    catch{
+        return false;
+    }
 }
 
 /**
@@ -38,6 +47,13 @@ function verifyJwt(token) {
  */
 function decodeJwt(token) {
     // Your code here
+
+    const decoded = jwt.decode(token);
+    if(decoded) {
+        return true;
+    }
+
+    return false;
 }
 
 
